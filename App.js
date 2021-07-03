8649291c4a86d6553773f852f8b82f62e6d32c91
evanstech12345 <evanshcode@gmail.com>
2021-07-03 15:28:43 -0400

    navigation error

--- App.js
+++ App.js
@@ -1,23 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Platform ,Alert ,Button, StyleSheet, Text,TouchableWithoutFeedback, View, Image,  SafeAreaView, TouchableOpacity, } from 'react-native';
+import { Platform ,Alert ,Button, StyleSheet, Text,TouchableWithoutFeedback, View, Image,  SafeAreaView, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+// import { navigationRef } from './RootNavigation';
 
-export default function App() {
-  const textPress = () => console.log('text pressed')
-  
+
+
+
+function DetailsScreen() {
   return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
+
+
+export default function HomeScreen({navigation}) {
+  const textPress = () => console.log('text pressed')
+  // const navigation = useNavigation(); 
+  return (
+    
     <SafeAreaView style={styles.container}>
       {/* <Text onPress={textPress}>Welcome to Moodic</Text> */}
       <Image 
       style={styles.logo}
       source={require('./assets/logo.jpeg')} />
-      <TouchableOpacity
+      {/* <TouchableOpacity
         style={styles.happyButton}
-        onPress={() => console.log('your happy')}
+        onPress={() => navigation.navigate('Details')}
         >
           <Text style={styles.textHappy} >Feeling Happy</Text>
-        </TouchableOpacity>
-
+        </TouchableOpacity> */}
+        <Button 
+        title="Feeling Happy" 
+        style={styles.happyButton}
+        onPress={() => navigation.navigate('Details')}
+        backgroundColor="blue"
+        />
         <TouchableOpacity
         style={styles.sadButton}
         onPress={() => console.log('your happy')}
